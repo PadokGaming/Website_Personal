e81ebf5387724806b2d48d300e0882a2fa1dc8a7
PadokGaming <140513837+PadokGaming@users.noreply.github.com>
2023-07-25 19:20:03 +0700

    Add files via upload

---
+++ profilWeb.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wartzy ( Waritz Alfarisy) Personal Website</title>
+    <style>
+        header
+        {
+            background-color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <br>
+    <header style="text-align: center;">
+        <img src="foto.jpg"  width="200" height="200" style="border-radius: 50%;"/>
+        <h1>Waritz Alfarisy</h1>
+        <p>(Web Developer)</p>
+    </header>
+
+    <hr />
+
+    <article style="text-align: center;">
+        <h2>Overview</h2>
+        <p>
+            Hi, saya Waritz Alfarisy, saya adalah seorang mahasiswa yang sedang belajar menjadi web developer yang berdomisisli di Sambas.
+            Saat ini sedang berkuliah di POLTESA dan sedang mengikuti pelatihan Juior Web Developer.
+        </p>
+    </article>
+
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr style="background-color: cyan;">
+                    <th>Skill</th>
+                    <th>Pengalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML (Pro)</li>
+                            <li>CSS (Newbie)</li>
+                            <li>C++ (Pro)</li>
+                            <li>Sublime Text (Pro)</li>
+                            
+
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Freelancer di Internet</li>
+                           <li>Penginput Data Di Kedinasan</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright &copy; 2023 Waritz Alfarisy.</p>
+    </footer>
+</body>
+</html>
